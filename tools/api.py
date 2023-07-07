b9c6e993ccd781762153a58cfb4299ef4c01aebf
Hitoshi Yoshida <github@peria.jp>
2023-07-07 22:33:57 +0900

    Support problems command, and can get new problems

--- tools/api.py
+++ tools/api.py
@@ -2,11 +2,13 @@
 
 import os
 import sys
+import json
 import requests
 
 URL_DOMAIN = "api.icfpcontest.com"
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-DATA_DIR = f"{BASE_DIR}/data/problems"
+DATA_DIR = f"{BASE_DIR}/data"
+PROBLEMS_DIR = f"{DATA_DIR}/problems"
 
 
 def command_problem(ids):
@@ -17,7 +19,7 @@ def command_problem(ids):
     print(BASE_DIR)
 
     for id in ids:
-        filepath = f"{DATA_DIR}/problem-{id}.json"
+        filepath = f"{PROBLEMS_DIR}/problem-{id}.json"
         if os.path.isfile(filepath):
             continue
 
@@ -33,10 +35,36 @@ def command_problem(ids):
     return True
 
 
+def command_problems(options):
+    # problems
+    # api.py problems --get # get new problems
+    url = f"https://{URL_DOMAIN}/problems"
+    response = requests.get(url)
+    data = response.text
+    data = data.replace("\\n", '\n')
+    data = data.replace('\\"', '"')
+    data = json.loads(data)
+    number = data['number_of_problems']
+    print(f"Number of problems: {number}")
+
+    if '--get' in options:
+        current = sum(os.path.isfile(os.path.join(PROBLEMS_DIR, name))
+                      for name in os.listdir(PROBLEMS_DIR))
+        if current < number:
+            print(
+                "Currently we have only {current} problems. Download unavailable problems.")
+            command_problem(list(range(1, number + 1)))
+
+    return True
+
+
 def main():
     subcommand = "" if len(sys.argv) < 2 else sys.argv[1]
     if subcommand == "problem":
         if command_problem(sys.argv[2:]):
+            return
+    elif subcommand == "problems":
+        if command_problems(sys.argv[2:]):
             return
 
     print("Usage: " + sys.argv[0] + " subcommand", file=sys.stderr)
