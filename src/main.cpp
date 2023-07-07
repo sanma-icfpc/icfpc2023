5352c0943501e5415c301ea7c785051c0a1ee55f
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2023-07-08 04:51:06 +0900

    add solution-to-png subcommand

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,33 @@ int main(int argc, char* argv[]) {
   sub_problem_to_png->add_option("problem_file", problem_file, "problem file path (JSON)");
   sub_problem_to_png->add_option("output_file", output_file, "output file path (PNG)");
 
+  auto sub_solution_to_png = app.add_subcommand("solution-to-png");
+  std::string solution_file;
+  sub_solution_to_png->add_option("solution_file", solution_file, "solution file path (JSON)");
+  sub_solution_to_png->add_option("output_file", output_file, "output file path (PNG)");
+
   CLI11_PARSE(app, argc, argv);
 
-  if (sub_problem_to_png) {
 #ifndef HAVE_OPENCV_HIGHGUI
     LOG(ERROR) << "no OpenCV highgui!";
 #else
+  if (sub_problem_to_png->parsed()) {
     Problem problem = Problem::from_file(problem_file);
     cv::Mat img = problem.to_mat();
     cv::imwrite(output_file, img);
-#endif
   }
+  if (sub_solution_to_png->parsed()) {
+    if (auto problem_id = guess_problem_id(solution_file)) {
+      Problem problem = Problem::from_file(*problem_id);
+      Solution solution = Solution::from_file(solution_file);
+      cv::Mat img = solution.to_mat(problem);
+      cv::imwrite(output_file, img);
+    } else {
+      LOG(ERROR) << "Could not guess the problem id!";
+      return 1;
+    }
+  }
+#endif
 
 
   return 0;
