32716762261b9f41adad8187d26d49938e18e14d
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2023-07-09 13:15:20 +0900

    fix CI settings

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 bash libs/build.sh || exit 1
-make -j8 -B -C src dirs solver test || exit 1
+make -j8 -B -C src dirs solver k3solver tssolver test || exit 1
 
 mkdir -p build || exit 1
 cp src/solver build/main || exit 1
--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#./build.sh || exit 1
+./build.sh || exit 1
 # HACK: VS2022 と WSL/MSYS2 で、フォルダの位置を合わせる。
-pushd vs/test
-rm -f test_result_linux.xml
-../../src/test --gtest_output=xml:test_result_linux.xml || exit 1
+pushd vs/
+rm -f test/test_result_linux.xml
+../src/test --gtest_output=xml:test/test_result_linux.xml || exit 1
 popd
