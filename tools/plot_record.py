ecc379d5726e026487076cfebd20a1cc83725c12
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2023-07-09 23:55:10 +0900

    plot_record: D&D file

--- tools/plot_record.py
+++ tools/plot_record.py
@@ -1,25 +1,28 @@
-# streamlit run plot_record.py <path/to/log>
+# > pip install streamlit pandas
+# > streamlit run plot_record.py <path/to/log>
+#
+#
 # log is like:
 # google::InitGoogleLogging(argv[0]);
 # google::InstallFailureSignalHandler();
 # google::SetStderrLogging(google::INFO);
 # google::SetLogDestination(google::INFO, "tssolver.log.");
 #
-# LOG(INFO) << format(R"(RECORD {"loop": %d, "best":%lld, "accept":%d, "reject":%d})", loop, best_score, accept, reject);
+# LOG(INFO) << format(R"(RECORD {"loop": %d, "best":%lld, "current":%lld, "accept":%d, "reject":%d})", loop, best_score, score, accept, reject);
 import streamlit as st
 import pandas as pd
-import numpy as np
-import sys
+import io
 import datetime
 import re
 import json
 
 pattern = re.compile(r'^(?P<level>.)(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})\s*(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})\.(?P<microsecond>\d+).*?RECORD (?P<json>.+)$')
 
-file_path = sys.argv[1]
-started_at = None
-rows = []
-with open(file_path, 'r') as fi:
+uploaded_file = st.file_uploader("Choose a log file")
+if uploaded_file is not None:
+    fi = io.StringIO(uploaded_file.getvalue().decode("utf-8"))
+    started_at = None
+    rows = []
     for line in fi.readlines():
         mo = pattern.match(line)
         if mo is not None:
@@ -43,8 +46,15 @@ with open(file_path, 'r') as fi:
             rows.append(payload)
             print(payload)
 
-df = pd.DataFrame(rows)
-df.set_index('time_s', inplace=True)
-st.line_chart(df[['best']])
-st.line_chart(df[['accept', 'reject']])
+    df = pd.DataFrame(rows)
+    df.set_index('time_s', inplace=True)
+
+    score_columns = []
+    if 'best' in df.columns: score_columns.append('best')
+    if 'current' in df.columns: score_columns.append('current')
+    st.line_chart(df[score_columns])
+    if 'accept' in df.columns and 'reject' in df.columns:
+        st.line_chart(df[['accept', 'reject']])
+    if 'T' in df.columns:
+        st.line_chart(df[['T']])
 
