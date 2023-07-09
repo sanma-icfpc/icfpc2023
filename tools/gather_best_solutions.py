2f98476a2048e17e79e88f4b5253f00dfb68f117
komori3 <39507080+komori3@users.noreply.github.com>
2023-07-09 23:30:17 +0900

    --set-constant-volumes

--- tools/gather_best_solutions.py
+++ tools/gather_best_solutions.py
@@ -3,6 +3,7 @@
 import os
 import re
 import glob
+import json
 import shutil
 import argparse
 import subprocess
@@ -24,12 +25,11 @@ def eval_solution(solution_path):
     res = subprocess.run(cmds, cwd=cwd, check=True, capture_output=True, text=True)
     return int(res.stdout.split('=')[1].strip().split(' ')[0])
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('target_dir', nargs='?', type=str, default=os.path.join(SOLUTIONS_DIR, 'bests'))
     parser.add_argument('--solutions-dir', type=str, default=SOLUTIONS_DIR)
-    # parser.add_argument('--set-constant-volume', type=float, default=-1.0)
+    parser.add_argument('--set-constant-volumes', type=float, default=-1.0)
     args = parser.parse_args()
 
     assert os.path.isdir(args.solutions_dir)
@@ -70,9 +70,17 @@ def main():
         os.makedirs(args.target_dir)
 
     for id, path in id_to_best_path.items():
-        basename = os.path.basename(path)
-        shutil.copy2(path, os.path.join(args.target_dir, basename))
-    
+        basename = f'solution-{id}.json'
+        target_path = os.path.join(args.target_dir, basename)
+        if args.set_constant_volumes < 0:
+            shutil.copy2(path, target_path)
+        else:
+            with open(path, 'r', encoding='utf-8') as f:
+                solution = json.load(f)
+            num_musicians = len(solution['placements'])
+            solution['volumes'] = [args.set_constant_volumes] * num_musicians
+            with open(target_path, 'w', encoding='utf-8') as f:
+                json.dump(solution, f, indent=4)
 
 
 if __name__ == '__main__':
