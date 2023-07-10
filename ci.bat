dde217f6802d5c774eaa9cc0c635e8e5337668e2
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2023-07-10 10:09:07 +0900

    break CI if build failed (Windows)

--- ci.bat
+++ ci.bat
@@ -1,4 +1,4 @@
-CALL build.bat
+CALL build.bat || EXIT /B 1
 PUSHD vs
 if exist test\test_result_windows.xml del test\test_result_windows.xml
 x64\Release\test.exe --gtest_output=xml:test\test_result_windows.xml || EXIT /B 1
